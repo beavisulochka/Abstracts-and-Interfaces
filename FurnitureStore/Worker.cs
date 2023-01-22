4c40f93d3fa8e0757544374902319b0e92276725
beavisulochka <102587784+beavisulochka@users.noreply.github.com>
2023-01-22 17:14:49 +0100

    Update Worker.cs

--- FurnitureStore/Worker.cs
+++ FurnitureStore/Worker.cs
@@ -1,11 +1,11 @@
 ﻿using System;
 namespace FurnitureStore
 {
-	public abstract class Worker
-	{
-		public string Name { get; }
-		public int Age { get; set; }
-		public string Vacancy { get; set; }
-	}
+    public abstract class Worker
+    {
+	public string Name { get; }
+	public int Age { get; set; }
+	public string Vacancy { get; set; }
+    }
 }
 
